ea9930cd30c722ce63bb27eaca478e76122d0cfe
Eddy <eddypv0@gmail.com>
2021-07-13 18:00:45 -0400

    [UPDATE] Modify About Component

--- src/App.js
+++ src/App.js
@@ -4,24 +4,8 @@ import Footer from './components/Footer'
 import AnecdoteList from './components/AnecdoteList'
 import NewAnecdote from './components/NewAnecdote'
 import AnecdoteDetail from './components/AnecdoteDetail'
+import About from './components/About'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-
-
-
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -43,12 +27,9 @@ const App = () => {
 
   const [notification, setNotification] = useState('')
   const addNew = (anecdote) => {
-    
     anecdote.id = (Math.random() * 10000).toFixed(0)
     setAnecdotes(anecdotes.concat(anecdote))
-    setNotification(`a new anecdote "${anecdote.content}" created!`)
-    
-
+    setNotification(`a new anecdote "${anecdote.content}" created!`)    
   }
 
   const anecdoteById = (id) =>
@@ -65,16 +46,6 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
-  /*return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
-  )*/
   return(
     <div>
       <h1>Software anecdotes</h1>
@@ -86,6 +57,9 @@ const App = () => {
           </Route>
           <Route path="/anecdotes/:id">
             <AnecdoteDetail anecdotes={anecdotes} />
+          </Route>
+          <Route path="/about">
+            <About />
           </Route>
           <Route path="/"> 
             <AnecdoteList anecdotes={anecdotes} />
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ const Menu = () => {
     <div>
       <Link style={padding} to="/">anecdotes</Link>
       <Link style={padding} to="/create">create new</Link>
-      <Link style={padding} to="/">about</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
