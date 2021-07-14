472647afcf6993356785a2bf88779aab6947d669
Eddy <eddypv0@gmail.com>
2021-07-14 10:03:57 -0400

    [UPDATE] Add custom hook useField in NewAnecdote component

--- src/components/NewAnecdote.js
+++ src/components/NewAnecdote.js
@@ -1,17 +1,18 @@
 import { useState } from "react"
 import { useHistory } from 'react-router'
-const NewAnecdote = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-  const history = useHistory()
+import { useField } from "../hooks"
 
+const NewAnecdote = (props) => {
+  const history = useHistory()
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content:content.value,
+      author:author.value,
+      info:info.value,
       votes: 0
     })
     history.push('/')
@@ -24,15 +25,15 @@ const NewAnecdote = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content.value} onChange={content.onChange} type={content.type} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author.value} onChange={author.onChange} type={author.type} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info.value} onChange={info.onChange} type={info.type} />
         </div>
         <button>create</button>
       </form>
